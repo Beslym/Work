064543ce0b673192b723c977abeef1090166ac7e
Beslym <78370238+Beslym@users.noreply.github.com>
2021-02-26 02:26:23 +0300

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,31 @@
+package geekbrains.lesson_Guess_HomeWork;
+
+public class Cat extends Animal{
+
+
+    public Cat(int maxRun, double maxJump, int canSwim) {
+        super(maxRun, maxJump, canSwim);
+    }
+
+    @Override
+    boolean run(int runLenght) {
+        if (runLenght > 0 && runLenght <= maxRunLenght)
+            return true;
+        System.out.println("Incorrect run lenght of your cat - " + runLenght);
+        return false;
+    }
+
+    @Override
+    boolean jump(double jumpHeight) {
+        if (jumpHeight > 0 && jumpHeight <= maxJumpHeight)
+            return true;
+        System.out.println("Incorrect jump height of your cat - " + jumpHeight);
+        return false;
+    }
+
+    @Override
+    boolean canSwim(int swimDistance) {
+        System.out.println("Cat is can't swim!");
+        return false;
+    }
+}
