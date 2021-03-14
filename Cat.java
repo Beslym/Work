18926e3c1dff79564c292dbf0ff51bae8fe93e5e
Beslym <78370238+Beslym@users.noreply.github.com>
2021-03-15 00:53:18 +0300

    Lesson2.1

---
+++ Cat.java
@@ -0,0 +1,19 @@
+package geekbrains.lesson_Guess_HomeWork.less21;
+
+public class Cat implements Competitor, Run, Jump {
+    private final int maxrun;
+    private final int maxjump;
+
+    public Cat(int maxrun, int maxjump) {
+        this.maxrun = maxrun;
+        this.maxjump = maxjump;
+    }
+    public boolean jump(int distance) {
+        System.out.println("Cat attempts jump!");
+        return maxjump >= distance;
+    }
+    public boolean run(int distance) {
+        System.out.println("Cat attempts run!");
+        return maxrun >= distance;
+    }
+}
---
+++ Wall.java
@@ -0,0 +1,18 @@
+package geekbrains.lesson_Guess_HomeWork.less21;
+
+public class Wall implements Obstacle {
+    private final int height;
+
+    @Override
+    public boolean actWithMe(Competitor team) {
+        if (team instanceof Jump) {
+            return ((Jump) team).jump(height);
+        } else {
+            System.out.println("Cannot run");
+            return false;
+        }
+    }
+    public Wall(int height) {
+        this.height = height;
+    }
+}
