0810297e02da7afbbb1273a3c8257cb31f80b37d
Beslym <78370238+Beslym@users.noreply.github.com>
2021-04-12 11:06:56 +0300

    Add files via upload

---
+++ Orange.java
@@ -0,0 +1,11 @@
+package geekbrains.lesson_Guess_HomeWork;
+
+public class Orange extends Fruit {
+
+    private float weight = 1.5f;
+
+    @Override
+    public float getWeight() {
+        return weight;
+    }
+}
