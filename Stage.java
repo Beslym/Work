81be94dd5b9f1ea9645de2b882d5c21f8e16fb95
Beslym <78370238+Beslym@users.noreply.github.com>
2021-05-03 05:25:23 +0300

    Add files via upload

---
+++ Stage.java
@@ -0,0 +1,10 @@
+package geekbrains.lesson_Guess_HomeWork;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+    public String getDescription() {
+        return description;
+    }
+    public abstract void go(Car c);
+}
